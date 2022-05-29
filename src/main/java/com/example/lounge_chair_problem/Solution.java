782714b6021ad96b959922b8d91670d6cff47985
Nurjigit <alymbekovnurjigit01@gmail.com>
2022-05-30 01:41:50 +0600

    Added comments for methods in Solution.java class

--- src/main/java/com/example/lounge_chair_problem/Solution.java
+++ src/main/java/com/example/lounge_chair_problem/Solution.java
@@ -3,6 +3,9 @@ import javafx.collections.ObservableList;
 import java.util.Objects;
 
 public class Solution {
+    /*
+    * Place_new_group. Pls don't give much attention to names of methods. This method is used to find and locate a new group of customers into right chairs. Inside this method I used a method for clustering, to know where we should look for chairs for a new group of customers. Then algorithm at first looks for sequence of chairs that the size of sequence will match to size of group of customers. If it finds it locates the group on chairs. If there is no such a sequence of chairs algorithm will look for the sequences of chairs that will return true for the next condition current_group.size() < sequence_of_chairs.size()
+    */
     public void Place_New_Group(ObservableList<Chairs_Group> chairs_groups, Chairs_Group new_group)
     {
         int tmp_index = Clustering_of_chairs(chairs_groups);
@@ -33,6 +36,9 @@ public class Solution {
         }
     }
 
+    /*
+     * Remove_Group. This method is for cases when the group wanted to return to home. The chairs that they used after calling this method will be free. Method removes the group by their unique id.
+     */
     public void Remove_Group(ObservableList<Chairs_Group> chairs_groups, int outgoing_group_index)
     {
         for(Chairs_Group ch: chairs_groups)
@@ -66,6 +72,9 @@ public class Solution {
         Circlize(chairs_groups);
     }
 
+    /*
+     * Circlize. Since the chairs located around a pool like a circle, data structure that presents all chairs should be also like a circle. What it means? Imagine the case that the first and the last element in our list of chairs present a free chairs, since chairs located around a pool like a circle, we need to circlize the list, by circlize I mean appending of the first element to last element, so we can get a list which starts by an element that presents already taken chair and ends by an element which presents a free chair.
+     */
     public void Circlize(ObservableList<Chairs_Group> chairs_groups)
     {
         if(chairs_groups.size() > 1)
@@ -78,6 +87,10 @@ public class Solution {
         }
     }
 
+    /*
+     * Clustering_of_chairs. In this method I tried to implement a clustering algorithm for 1 dimensional lists. The goal that I want to achieve with this method is to find the biggest cluster of chairs that are already taken. There you can ask a question why? Because the optimal way to allocate chairs for a new group of customers. so we can earn more money is to give them a chairs that are near the biggest cluster or inside biggest cluster the second case is the best for us.
+     * The size of a range to search for a cluster of non-free chairs is equal to a sum of all non-free chairs.
+     */
     public int Clustering_of_chairs(ObservableList<Chairs_Group> chairs_groups)
     {
         int cnt = 0;
